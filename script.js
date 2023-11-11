decd92e907e7885c51f959f2530e4de67c87dd0e
GurtejSinghBedi <144299784+GurtejSinghBedi@users.noreply.github.com>
2023-11-11 20:14:29 +0530

    toppr-website-clone

---
+++ script.js
@@ -0,0 +1,15 @@
+function function1(){
+    document.querySelector('#opt1').style.border='2px solid rgb(34,139,34)';
+}
+
+function function2(){
+    document.querySelector('#opt2').style.border='2px solid rgb(255,87,51)';
+}
+
+function function3(){
+    document.querySelector('#opt3').style.border='2px solid rgb(255,87,51)';
+}
+
+function function4(){
+    document.querySelector('#opt4').style.border='2px solid rgb(255,87,51)';
+}
